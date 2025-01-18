5c473bb54a768ad0b3fe9020d83084d77d80c3bb
Rishabh ojha <rishabhojha136@gmail.com>
2025-01-18 14:52:30 +0530

    bserch added

--- searching.py
+++ searching.py
@@ -11,3 +11,25 @@ if result !=-1:
     print("element found")
 else:
     print("not found")
+
+#binary search
+def bsearch(list1,n1):
+    l=0
+    h=len(list1)
+    while l<=h:
+        mid=(l+h) //2
+        if list1[mid]>n1:
+            h=mid-1
+        elif list1[mid]<n1:
+            l =mid+1
+        else:
+            return mid
+    return -1
+
+list1=[2,4,6,7,8,22,25,100]
+n1=22
+result1=ser(list1,n1)
+if result1 !=-1:
+    print("element found",result1)
+else:
+    print("not found")
