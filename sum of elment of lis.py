9c38bf3174204d82205245be80e702aee904cccf
Rishabh ojha <rishabhojha136@gmail.com>
2025-01-18 21:20:40 +0530

    multiple feature is added

--- sum of elment of lis.py
+++ sum of elment of lis.py
@@ -5,9 +5,15 @@ def sumoflist(lis):
         sum = sum + list[i]
     return sum
 
+def muloflist(list):
+    mul=1
+    for i in range(len(list)):
+        mul = mul * list[i]
+    return mul
 
 
 list=[2,3,5,10,15]
 
 print("total value",sumoflist(list))
+print("Mul value",muloflist(list))
 
